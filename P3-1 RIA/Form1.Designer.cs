2a1bc469e2f1160a821427d55d9cc2597b239f54
Ruth Ivorra Albert <rutivoalb@alu.edu.gva.es>
2022-12-05 18:28:44 +0100

    Segundo commit: código inicial con errores

--- P3-1 RIA/Form1.Designer.cs
+++ P3-1 RIA/Form1.Designer.cs
@@ -28,12 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtTelegrama = new System.Windows.Forms.TextBox();
+            this.lTexto = new System.Windows.Forms.Label();
+            this.cbUrgente = new System.Windows.Forms.CheckBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.bCalcular = new System.Windows.Forms.Button();
+            this.lCoste = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtTelegrama
+            // 
+            this.txtTelegrama.Location = new System.Drawing.Point(98, 40);
+            this.txtTelegrama.Multiline = true;
+            this.txtTelegrama.Name = "txtTelegrama";
+            this.txtTelegrama.Size = new System.Drawing.Size(558, 205);
+            this.txtTelegrama.TabIndex = 0;
+            // 
+            // lTexto
+            // 
+            this.lTexto.AutoSize = true;
+            this.lTexto.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lTexto.Location = new System.Drawing.Point(98, 9);
+            this.lTexto.Name = "lTexto";
+            this.lTexto.Size = new System.Drawing.Size(39, 15);
+            this.lTexto.TabIndex = 1;
+            this.lTexto.Text = "Texto";
+            // 
+            // cbUrgente
+            // 
+            this.cbUrgente.AutoSize = true;
+            this.cbUrgente.Location = new System.Drawing.Point(83, 266);
+            this.cbUrgente.Name = "cbUrgente";
+            this.cbUrgente.Size = new System.Drawing.Size(73, 19);
+            this.cbUrgente.TabIndex = 2;
+            this.cbUrgente.Text = "Urgente?";
+            this.cbUrgente.UseVisualStyleBackColor = true;
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(203, 336);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(162, 23);
+            this.txtPrecio.TabIndex = 3;
+            // 
+            // bCalcular
+            // 
+            this.bCalcular.Location = new System.Drawing.Point(465, 286);
+            this.bCalcular.Name = "bCalcular";
+            this.bCalcular.Size = new System.Drawing.Size(191, 73);
+            this.bCalcular.TabIndex = 4;
+            this.bCalcular.Text = "Calcular";
+            this.bCalcular.UseVisualStyleBackColor = true;
+            this.bCalcular.Click += new System.EventHandler(this.bCalcular_Click);
+            // 
+            // lCoste
+            // 
+            this.lCoste.AutoSize = true;
+            this.lCoste.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lCoste.Location = new System.Drawing.Point(98, 339);
+            this.lCoste.Name = "lCoste";
+            this.lCoste.Size = new System.Drawing.Size(38, 15);
+            this.lCoste.TabIndex = 5;
+            this.lCoste.Text = "Coste";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lCoste);
+            this.Controls.Add(this.bCalcular);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.cbUrgente);
+            this.Controls.Add(this.lTexto);
+            this.Controls.Add(this.txtTelegrama);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private TextBox txtTelegrama;
+        private Label lTexto;
+        private CheckBox cbUrgente;
+        private TextBox txtPrecio;
+        private Button bCalcular;
+        private Label lCoste;
     }
 }
